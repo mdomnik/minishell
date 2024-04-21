6bf01e32afa19a10fff31721fcda3928c92e8c90
Maciej Domnik <mdomnik.maciej@gmail.com>
2024-04-22 00:21:44 +0200

    signals

--- src/main/signals.c
+++ src/main/signals.c
@@ -6,7 +6,7 @@
 /*   By: mdomnik <mdomnik@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 19:13:52 by mdomnik           #+#    #+#             */
-/*   Updated: 2024/04/21 20:34:09 by mdomnik          ###   ########.fr       */
+/*   Updated: 2024/04/21 20:38:42 by mdomnik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,16 @@ void	handle_sigint(int sig)
 	}
 }
 
+void	handle_sigquit(int sig)
+{
+	if (sig == SIGQUIT)
+	{
+		printf("Quit: 3\n");
+	}
+}
+
 void	set_signals(void)
 {
 	signal(SIGINT, handle_sigint);
+	signal(SIGQUIT, SIG_IGN);
 }
