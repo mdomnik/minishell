162d25b83b8bba0c3056522bbfa5462e18864413
Maciej Domnik <mdomnik.maciej@gmail.com>
2024-05-02 17:48:11 +0200

    changes

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mdomnik <mdomnik@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 21:53:11 by kaan              #+#    #+#             */
-/*   Updated: 2024/04/30 18:11:44 by mdomnik          ###   ########.fr       */
+/*   Updated: 2024/05/02 14:29:17 by mdomnik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,7 @@
 # define ERR_CMD "Error: command not found"
 # define ERR_NCMD ": command not found"
 # define ERR_PWD "Error: failed to get current working directory"
+# define ERR_PWDA "pwd: too many arguments"
 # define ERR_ARG "Error: too many arguments"
 # define ERR_CD " No such file or directory"
 # define ERR_PATH "Error: failed to find path"
